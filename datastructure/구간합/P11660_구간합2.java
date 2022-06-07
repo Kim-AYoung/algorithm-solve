0dcf804926b6aed9191bd13f7fc4d1fe1785d697
Kim-AYoung <onionlily123@naver.com>
2022-06-08 00:21:30 +0900

    [자료구조-구간합] 구간합2-마지막 풀이

--- datastructure/구간합/P11660_구간합2.java
+++ datastructure/구간합/P11660_구간합2.java
@@ -5,45 +5,36 @@ import java.util.StringTokenizer;
 
 public class P11660_구간합2 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringBuilder sb = new StringBuilder();
 
-        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int questionNum = Integer.parseInt(stringTokenizer.nextToken());
-
-        // 각 행 마다 구간 합 배열 구하기
-        long accumulatedSumEachRow[][] = new long[N][N];
-        for(int i=0; i<N; i++) {
-            stringTokenizer = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
-                int num = Integer.parseInt(stringTokenizer.nextToken());
-                accumulatedSumEachRow[i][j] =
-                        j==0 ?
-                                num :
-                                accumulatedSumEachRow[i][j-1] + num;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int A[][] = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        // 구간 합 구하기
-        for(int i=0; i<questionNum; i++) {
-            stringTokenizer = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(stringTokenizer.nextToken())-1;
-            int y1 = Integer.parseInt(stringTokenizer.nextToken())-1;
-            int x2 = Integer.parseInt(stringTokenizer.nextToken())-1;
-            int y2 = Integer.parseInt(stringTokenizer.nextToken())-1;
-
-            long intervalSumForAllRow = 0;
-            for(int j=x1; j<=x2; j++) {
-                long intervalSum =
-                        y1==0 ?
-                                accumulatedSumEachRow[j][y2] :
-                                accumulatedSumEachRow[j][y2]-accumulatedSumEachRow[j][y1-1];
-                intervalSumForAllRow += intervalSum;
+        int D[][] = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                //부분 합 구하기
+                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
             }
-            sb.append(intervalSumForAllRow).append("\n");
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            //  부분 합 배열을 이용한 질의 답변하기
+            int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+            sb.append(result).append("\n");
         }
 
         bw.write(String.valueOf(sb));
