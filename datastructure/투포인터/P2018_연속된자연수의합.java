f3eb89cedf0b7cd3def16f96ece10d6dd4b14669
Kim-AYoung <onionlily123@naver.com>
2022-06-13 21:55:02 +0900

    [자료구조-투포인터] 연속된자연수의합-두번째정답

--- datastructure/투포인터/P2018_연속된자연수의합.java
+++ datastructure/투포인터/P2018_연속된자연수의합.java
@@ -7,23 +7,25 @@ public class P2018_연속된자연수의합 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        long temp = N/2;
-        long count = 0; // 연속된 자연수의 합 = N을 만족하는 가짓수
-        for(long i=0; i<temp; i++) { // i : 연속된 합의 시작 인덱스
-            long c = -(i*i + i + 2L*N); // 이차방정식의 상수항
-            double positiveRealRoot = quadraticFormula(c);
-            //System.out.println((i+1) + "~" + positiveRealRoot);
-            count += isNaturalNumber(positiveRealRoot) ? 1 : 0;
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+
+        while(end_index != N) {
+            if(sum == N) {
+                count++;
+                end_index++;
+                sum += end_index;
+            }else if(sum > N) {
+                sum -= start_index;
+                start_index++;
+            }else {
+                end_index++;
+                sum += end_index;
+            }
         }
-        System.out.println(count + 1);
-    }
 
-    // 근의 공식
-    private static double quadraticFormula(long c) {
-        return (-1.0 + Math.sqrt(1-4*c))/2.0;
-    }
-
-    private static boolean isNaturalNumber(double realNumber) {
-       return realNumber == (((long) realNumber) * 1.0);
+        System.out.println(count);
     }
 }
