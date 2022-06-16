44d30792a36752a8d0bbe8711008cd073fe942b0
Kim-AYoung <onionlily123@naver.com>
2022-06-17 00:41:45 +0900

    [자료구조-슬라이딩윈도우] 최솟값찾기-첫번째정답, 백준 시간초과로 인해 좀 더 Fast하게 바꿈

--- datastructure/슬라이딩윈도우/P11003_최솟값찾기.java
+++ datastructure/슬라이딩윈도우/P11003_최솟값찾기.java
@@ -1,20 +1,20 @@
 package datastructure.슬라이딩윈도우;
 
 import java.io.*;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class P11003_최솟값찾기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb =new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        Deque<Node> myDeque = new LinkedList<>();
+        st = new StringTokenizer(br.readLine(), " ");
+        ArrayDeque<Node> myDeque = new ArrayDeque<>();
 
         for(int i=0; i<N; i++) {
             int num = Integer.parseInt(st.nextToken());
@@ -22,16 +22,16 @@ public class P11003_최솟값찾기 {
             while(!myDeque.isEmpty() && myDeque.getLast().value > num) {
                 myDeque.removeLast();
             }
-            myDeque.addLast(new Node(num, i));
+            myDeque.offerLast(new Node(num, i));
             // remove
             if(myDeque.getFirst().index <= i-L) {
                 myDeque.removeFirst();
             }
-            bw.write(myDeque.getFirst().value + " ");
+            sb.append(myDeque.getFirst().value).append(" ");
         }
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
-        br.close();
     }
 
     static class Node {
