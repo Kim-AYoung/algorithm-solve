38e9a61dcee1d6c914beb8f5169df8e83b4469dd
Kim-AYoung <onionlily123@naver.com>
2022-06-29 21:57:26 +0900

    [프로그래머스] 최대공약수와최소공배수-두번째정답(유클리드 호제법 사용)

--- programmers/최대공약수와최소공배수.java
+++ programmers/최대공약수와최소공배수.java
@@ -9,33 +9,14 @@ public class 최대공약수와최소공배수 {
 
     private static int[] solution(int n, int m) {
         int[] answer = new int[2];
-        int more;
-        int less;
-        if(n>m) {
-            more = n;
-            less = m;
-        }else {
-            less = m;
-            more = n;
-        }
-
-        // 최대공약수
-        for(int i=less; i>=1; i--) {
-            if(less % i == 0 && more % i == 0) {
-                answer[0] = i;
-                break;
-            }
-        }
-        // 최소공배수
-        int multiple = more;
-        while(true) {
-            if(multiple % less == 0) {
-                answer[1] = multiple;
-                break;
-            }
-            multiple += more;
-        }
+        answer[0] = gcd(n, m);
+        answer[1] = (n * m) / answer[0];
 
         return answer;
+    }
+
+    private static int gcd(int n, int m) {
+        if(m == 0) return n;
+        else return gcd(m, n%m);
     }
 }
