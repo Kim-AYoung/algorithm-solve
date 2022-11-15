6c292ddd8ce874ec33c7a7cc20dd5b7cb0f9b75d
Kim-AYoung <onionlily123@naver.com>
2022-11-15 21:03:34 +0900

    refactor : change folder name from dynamicprogramming to dp

--- dp/P11727_2Xn타일링2.java
+++ dp/P11727_2Xn타일링2.java
@@ -1,4 +1,4 @@
-package dynamicprogramming;
+package dp;
 
 import java.util.Scanner;
 
--- dp/P1912_연속합.java
+++ dp/P1912_연속합.java
@@ -1,4 +1,4 @@
-package dynamicprogramming;
+package dp;
 
 import java.io.BufferedReader;
 import java.io.IOException;
