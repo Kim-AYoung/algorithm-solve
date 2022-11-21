b3eab094ef39ebf4bf8d559126326ec95d765e91
Kim-AYoung <onionlily123@naver.com>
2022-11-21 16:39:29 +0900

    refactor

--- ScannerExample.java
+++ ScannerExample.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Scanner_Example {
+public class ScannerExample {
 	private static final Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
