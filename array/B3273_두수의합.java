853b27db53dbad317d3a3a98b00e0787979d87a1
Kim-AYoung <onionlily123@naver.com>
2024-07-01 16:41:22 +0900

    [array]두수의합

---
+++ array/B3273_두수의합.java
@@ -0,0 +1,38 @@
+package array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 시간복잡도 = O(n)
+ */
+public class B3273_두수의합 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int aLength = Integer.parseInt(br.readLine());
+        int[] a = new int[aLength];
+        boolean[] isExistNum = new boolean[1_000_001];
+        int x;
+        int answer = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < aLength; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        x = Integer.parseInt(br.readLine());
+
+        for (int num : a) {
+            if (x - num >= 0 && x - num <= 1_000_001 && isExistNum[x - num]) {
+                answer++;
+            }
+
+            isExistNum[num] = true;
+        }
+
+        System.out.println(answer);
+    }
+}
