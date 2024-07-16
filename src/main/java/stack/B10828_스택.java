d9239fdf5dee6b0fe1643457c56b9b90efdf3810
Kim-AYoung <onionlily123@naver.com>
2024-07-16 10:56:03 +0900

    [stack]백준 10828 - 스택

---
+++ src/main/java/stack/B10828_스택.java
@@ -0,0 +1,49 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 문제 : https://www.acmicpc.net/problem/10828
+ * 시간복잡도 = O(N)
+ */
+public class B10828_스택 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int[] stack = new int[10000];
+        int top = 0;
+
+        StringBuilder sb = new StringBuilder();
+        Integer N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if (command.equals("push")) {
+                int X = Integer.parseInt(st.nextToken());
+                stack[top] = X;
+                top++;
+            } else if (command.equals("pop")) {
+                if (top == 0) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack[top - 1]).append("\n");
+                    top--;
+                }
+            } else if (command.equals("size")) {
+                sb.append(top).append("\n");
+            } else if (command.equals("empty")) {
+                sb.append(top == 0 ? 1 : 0).append("\n");
+            } else if (command.equals("top")) {
+                if (top == 0) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack[top - 1]).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
