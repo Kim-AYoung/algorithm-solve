b7a4247ef42e761ac8bb21b74fe072aab9ef308e
Kim-AYoung <onionlily123@naver.com>
2024-09-25 15:47:46 +0900

    [recursion]백준 1074 - Z 리팩토링

--- src/main/java/recursion/B1074_Z.java
+++ src/main/java/recursion/B1074_Z.java
@@ -10,51 +10,23 @@ import java.util.StringTokenizer;
  */
 public class B1074_Z {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int r;
-    static int c;
-    static int visitCount = -1;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        visit((int) Math.pow(2, N), 0, 0);
+        System.out.println(visitOrder((int) Math.pow(2, N), r, c));
     }
 
-    private static void visit(int size, int i, int j) {
-        if (size == 1) {
-            visitCount++;
-            if (i == r && j == c) {
-                System.out.println(visitCount);
-                System.exit(0);
-            }
-        }
+    private static int visitOrder(int size, int r, int c) {
+        if (size == 1) return 0;
 
         int half = size / 2;
-        if (i <= r && r < i + half && j <= c && c < j + half) {
-            visit(half, i, j);
-        } else {
-            visitCount += half * half;
-        }
-
-        if (i <= r && r < i + half && j + size / 2 <= c && c < j + half + half) {
-            visit(half, i, j + half);
-        } else {
-            visitCount += half * half;
-        }
-
-        if (i + +size / 2 <= r && r < i + half + half && j <= c && c < j + half) {
-            visit(half, i + half, j);
-        } else {
-            visitCount += half * half;
-        }
-
-        if (i + half <= r && r < i + half + half && j + size / 2 <= c && c < j + half + half) {
-            visit(half, i + half, j + half);
-        } else {
-            visitCount += half * half;
-        }
+        if (r < half && c < half) return visitOrder(half, r, c);
+        if (r < half && c >= half) return half * half + visitOrder(half, r, c - half);
+        if (r >= half && c < half) return 2 * half * half + visitOrder(half, r - half, c);
+        return 3 * half * half + visitOrder(half, r - half, c - half);
     }
 }
